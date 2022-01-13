d9c66b415be00d0370e2b75524c04d0865f0c86b
benjaminroest <benjamin9roest@gmail.com>
2022-01-13 16:39:57 +0100

    changed ascii art

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -6,34 +6,38 @@ vim.cmd "packadd dashboard-nvim"
 g.dashboard_default_executive = "telescope"
 
 local custom_header = {
-  "",
-  "",
-  "",
-  "",
-  " .^.                                         ",
-  " |o|    ___         _       _ __   ___       ",
-  ".\' \'.  | _ \\___  __| |_____| |\\ \\ / (_)_ __  ",
-  "|.-.|  |   / _ \\/ _| / / -_)  _\\ V /| | \'  \\ ",
-  "\'   \'  |_|_\\___/\\__|_\\_\\___|\\__|\\_/ |_|_|_|_|"
+"",
+"",
+"",
+"       /\\                                                                 ",
+"     .'  '.                                                                ",
+"    /======\\                                                              ",
+"   ;:.  _   ;     _____            _        ___      ___                   ",
+"   |:. (_)  |    |  __ \\          | |      | \\ \\    / (_)               ",
+"   |:.  _   |    | |__) |___   ___| | _____| |\\ \\  / / _ _ __ ___        ",
+"   |:. (_)  |    |  _  // _ \\ / __| |/ / _ | __\\ \\/ / | | '_ ` _ \\     ",
+"   ;:.      ;    | | \\ | (_) | (__|   |  __| |_ \\  /  | | | | | | |      ",
+" .' \\:.    / `.  |_|  \\_\\___/ \\___|_|\\_\\___|\\__| \\/   |_|_| |_| |_|",
+"/ .-'':._.'`-. \\                                                          ",
+"|/    /||\\    \\|                                                         ",
+"",
+"",
+"",
 }
 
 g.dashboard_custom_header = custom_header
 
 g.dashboard_custom_section = {
   a = {
-    description = {"  Find File                  SPC f f"},
+    description = {"  Find File                  "},
     command = "Telescope find_files"
   },
   b = {
-    description = {"  Recents                    SPC f o"},
+    description = {"  Recents                    "},
     command = "Telescope oldfiles"
   },
-  c = {
-    description = {"  New Buffer                 SPC g g"},
-    command = "enew"
-  },
   d = {
-    description = {"  Edit Configuration         SPC d c"},
+    description = {"  Edit Configuration         "},
     command = "e ~/.config/nvim/init.lua"
   }
 }
