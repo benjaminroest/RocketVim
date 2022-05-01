528506f93e9365f4fb1cb37273ffd9d9b6645ab3
benjaminroest <benjamin9roest@gmail.com>
2022-05-01 11:16:53 +0200

    changed to latest vim version

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -40,10 +40,6 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -68,7 +64,7 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
+    -- auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -80,15 +76,20 @@ nvim_tree.setup {
     number = false,
     relativenumber = false,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
+  actions = {
+    open_file = {
+      quit_on_open = false,
+      window_picker = { 
+        enable = false,
+      },
+    },
   },
+  -- root_folder_modifier = ":t",
+  -- show_icons = {
+    -- git = 1,
+    -- folders = 1,
+    -- files = 1,
+    -- folder_arrows = 1,
+    -- tree_width = 30,
+  -- },
 }
