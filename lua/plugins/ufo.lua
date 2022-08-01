ea2ab97021181a6643366caf9630d8e1fc08abc7
Benjamin Roest <benjamin@Benjamins-MacBook-Pro.local>
2022-08-01 18:05:34 +0200

    deleted dashboard related stuff

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -3,10 +3,6 @@ if not status_ok then
   return
 end
 
--- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-
 ufo.setup {
   provider_selector = function(bufnr, filetype, buftype)
     return { "treesitter", "indent" }
